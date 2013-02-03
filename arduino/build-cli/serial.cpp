14a9469178120826f32074976b58134f3308b892
Jay Vana <jsvana@mtu.edu>
2013-02-03 03:45:53 +0000

    Working to build on Linux

---
+++ arduino/build-cli/serial.cpp
@@ -0,0 +1,87 @@
+#include <Arduino.h>
+#include <QueueList.h>
+
+int incomingByte;
+boolean lights[10];
+QueueList<char> command;
+
+void setup() {
+  int i;
+
+  for (i = 0; i < 10; i++) {
+    lights[i] = false;
+    pinMode(i, OUTPUT);
+  }
+
+  Serial.begin(9600);
+  Serial.flush();
+	Serial.println("penis");
+}
+
+void loop() {
+}
+
+void serialEvent() {
+  char request;
+  char type;
+  char number;
+  
+  boolean error = false;
+  boolean result;
+  
+  if (Serial.available() > 0) {
+    command.push(Serial.read());
+    
+    if (command.count() >= 3) {
+      request = command.pop();
+      type = command.pop();
+      number = command.pop();
+     
+     if (request == 'r') {
+        if (number >= '0' && number <= '9') {
+          Serial.print("r");
+          Serial.print(type);
+          Serial.print(number - '0');
+          Serial.println(lights[number - '0'] ? "+" : "-");
+        } else {
+          Serial.println("e");
+        }
+      } else {
+        if (number >= '0' && number <= '9') {
+          result = toggle(type, number - '0', error);
+          
+          if (error) {
+            Serial.println("e");
+          } else {
+            Serial.print(type);
+            Serial.print(number);
+            Serial.println(result ? "+" : "-");
+          }
+        } else {
+          Serial.println("e");
+        }
+      }
+    }
+  }
+}
+
+boolean toggle(char type, int number, boolean &error) {
+  switch (type) {
+    case 'l':
+      if (number >= 0 && number <= 9) {
+        if (lights[number]) {
+          digitalWrite(number, LOW);
+          lights[number] = false;
+        } else {
+          digitalWrite(number, HIGH);
+          lights[number] = true;
+        }
+
+        return lights[number];
+      }
+      break;
+  }
+
+  error = true;
+}
+
--- arduino/serial.ino
+++ arduino/serial.ino
@@ -14,6 +14,7 @@ void setup() {
 
   Serial.begin(9600);
   Serial.flush();
+	Serial.println("penis");
 }
 
 void loop() {
